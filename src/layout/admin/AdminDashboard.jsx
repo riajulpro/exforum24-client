5f247f5a2128d9509b504da72753ce71529f23d1
RiaJul Pro <riajulpro@outlook.com>
2023-11-27 07:31:36 +0600

    Adding user and admin dashboard structure

--- src/layout/admin/AdminDashboard.jsx
+++ src/layout/admin/AdminDashboard.jsx
@@ -1,5 +1,47 @@
-const AdminDashboard = () => {
-  return <div></div>;
+import { NavLink, Outlet, Link, useLocation } from "react-router-dom";
+import "../users/UserDashboard.css";
+
+const UserDashboard = () => {
+  const location = useLocation();
+
+  const isActiveLink = (to) => {
+    return location.pathname === to;
+  };
+
+  return (
+    <div className="h-screen">
+      <div className="grid grid-cols-12">
+        <div className="h-screen col-span-2 bg-white shadow sticky top-0">
+          <div className="p-5 border-b text-center">
+            <Link to={"/"}>
+              <h4 className="text-2xl font-semibold">ExForum24</h4>
+              <h6 className="text-xs uppercase leading-3 tracking-[0.08rem]">
+                Admin Dashboard
+              </h6>
+            </Link>
+          </div>
+          <div className="flex flex-col">
+            <Link
+              to={"/admin-dashboard"}
+              className={`${isActiveLink("/admin-dashboard") && "active"}`}
+            >
+              Admin Profile
+            </Link>
+            <NavLink to={"/admin-dashboard/manage-users"}>Manage Users</NavLink>
+            <NavLink to={"/admin-dashboard/reported-comments"}>
+              Reported Comments
+            </NavLink>
+            <NavLink to={"/admin-dashboard/make-announcement"}>
+              Make Announcement
+            </NavLink>
+          </div>
+        </div>
+        <div className="col-span-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
-export default AdminDashboard;
+export default UserDashboard;
--- src/layout/users/UserDashboard.jsx
+++ src/layout/users/UserDashboard.jsx
@@ -1,5 +1,42 @@
+import { NavLink, Outlet, Link, useLocation } from "react-router-dom";
+import "./UserDashboard.css";
+
 const UserDashboard = () => {
-  return <div></div>;
+  const location = useLocation();
+
+  const isActiveLink = (to) => {
+    return location.pathname === to;
+  };
+
+  return (
+    <div className="h-screen">
+      <div className="grid grid-cols-12">
+        <div className="h-screen col-span-2 bg-white shadow sticky top-0">
+          <div className="p-3 border-b pb-5">
+            <Link to={"/"}>
+              <h4 className="text-2xl font-semibold">ExForum24</h4>
+              <h6 className="text-xs uppercase leading-3 tracking-[0.13rem]">
+                User Dashboard
+              </h6>
+            </Link>
+          </div>
+          <div className="flex flex-col">
+            <Link
+              to={"/user-dashboard"}
+              className={`${isActiveLink("/user-dashboard") && "active"}`}
+            >
+              My Profile
+            </Link>
+            <NavLink to={"/user-dashboard/add-post"}>Add Post</NavLink>
+            <NavLink to={"/user-dashboard/my-post"}>My Post</NavLink>
+          </div>
+        </div>
+        <div className="col-span-10">
+          <Outlet />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserDashboard;
